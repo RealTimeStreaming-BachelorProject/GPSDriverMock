b5489d7e3972d6c11fac2686fd2a163e60d70e22
omvk97 <olvan18@student.sdu.dk>
2021-02-15 22:17:29 +0100

    proper auth

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,12 @@ import io from "socket.io-client";
 import { env } from "process";
 import { username, password, route } from "./driverinfo";
 import { loginToApplication } from "./helpers/api";
-import { DELIVERY_START, DRIVER_INIT, NEW_COORDINATES } from "./socketevents";
+import {
+  DELIVERY_START,
+  LATENCY_RESULT,
+  NEW_COORDINATES,
+  PING,
+} from "./socketevents";
 import { v4 as uuidv4 } from "uuid";
 import routes from "./routes.json";
 import { listenForSignals } from "./helpers/shutdown";
@@ -12,10 +17,6 @@ const driverSerivceURL =
 const routename = env.ROUTENAME ?? "route1";
 const driverUpdateInterval = 3000;
 
-const socket = io(driverSerivceURL, {
-  reconnectionAttempts: 5,
-});
-
 const routeCoordinates = (routes as any)[routename]["coordinates"];
 
 // Create package uuids which in the real world would be generated somewhere else
@@ -23,11 +24,14 @@ for (var i = 0; i < 100; i++) {
   (route.packages as any).push(uuidv4());
 }
 
-let intervalID: NodeJS.Timeout;
+let coordinateIntervalID: NodeJS.Timeout;
 
-function continouslySendCoordinates(coordinates: any) {
+function continouslySendCoordinates(
+  coordinates: any,
+  socket: SocketIOClient.Socket
+) {
   let counter = 0;
-  intervalID = setInterval(() => {
+  coordinateIntervalID = setInterval(() => {
     if (counter >= coordinates.length) {
       counter = 0; // reset back to start of route
     }
@@ -40,30 +44,60 @@ function continouslySendCoordinates(coordinates: any) {
   }, driverUpdateInterval);
 }
 
-socket.on("connect", async () => {
-  console.log("🚀 Connected to server");
-  try {
-    // Get token when connecting
-    const token = await loginToApplication(username, password);
-    console.log("I am connected, initializing connection");
-    socket.emit(DRIVER_INIT, { jwt: token });
+let latencyIntervalID: NodeJS.Timeout;
+// https://socket.io/docs/v3/migrating-from-2-x-to-3-0/#No-more-%E2%80%9Cpong%E2%80%9D-event-for-retrieving-latency
+function latencyTest(socket: SocketIOClient.Socket) {
+  latencyIntervalID = setInterval(() => {
+    const start = Date.now();
+    socket.emit(PING, () => {
+      const latency = Date.now() - start;
+      socket.emit(LATENCY_RESULT, latency); // emit the result back to the server for metrics
+    });
+  }, 5000);
+}
 
-    console.log("I have initialized. Starting delivery route");
-    socket.emit(DELIVERY_START, { packages: route.packages });
+function configureEndpoints(socket: SocketIOClient.Socket) {
+  socket.on("connect", async () => {
+    console.log("🚀 Connected to server");
+    try {
+      console.log("Starting delivery route");
+      socket.emit(DELIVERY_START, { packages: route.packages });
 
-    continouslySendCoordinates(routeCoordinates);
-  } catch (error) {
-    console.log(error.message);
-  }
-});
+      continouslySendCoordinates(routeCoordinates, socket);
+      latencyTest(socket);
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
 
-socket.on("disconnect", () => {
-  console.log("I disconnected");
-  clearInterval(intervalID);
-});
+  socket.on("disconnect", () => {
+    console.log("I disconnected");
+    clearInterval(coordinateIntervalID);
+    clearInterval(latencyIntervalID);
+  });
 
-socket.on("connect_error", () => {
-  console.log("Can't connect to server");
-});
+  socket.on("connect_error", (err: any) => {
+    console.log(err);
+  });
 
-listenForSignals(socket);
+  listenForSignals(socket);
+}
+
+let token: string;
+(async () => {
+  token = await loginToApplication(username, password);
+})()
+  .catch((e) => {
+    // Deal with the fact the chain failed
+    console.log(e)
+  })
+  .then(() => {
+    const config: any = {
+      econnectionAttempts: 5,
+      auth: {
+        token: token,
+      },
+    };
+    const socket = io(driverSerivceURL, config);
+    configureEndpoints(socket);
+  });
--- src/socketevents.ts
+++ src/socketevents.ts
@@ -1,6 +1,7 @@
 export const NEW_COORDINATES = "new_coordinates";
 export const LATEST_COORDINATES = "latest_coordinates";
-export const DRIVER_INIT = "driver_init";
 export const DELIVERY_START = "delivery_start";
 export const SUBSCRIBE_TO_DRIVER = "subscribe_to_driver_coordinates"
 export const PACKAGE_DELIVERED = "package_delivered"
+export const PING = "ping"
+export const LATENCY_RESULT = "latency_result"
